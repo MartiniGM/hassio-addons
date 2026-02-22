d82469497f9fc27ff6f9b0050fd90be06801d0e3
Martin FHIR ADT <2826051+MartiniGM@users.noreply.github.com>
2026-02-22 06:15:56 +0100

    Add miner name to mining script execution

--- duino_miner_hassio_addon/run.sh
+++ duino_miner_hassio_addon/run.sh
@@ -4,8 +4,9 @@
 USER=$(bashio::config 'username')
 KEY=$(bashio::config 'mining_key')
 CORES=$(bashio::config 'cores')
+MINER=$(bashio::config 'miner_name')
 
 echo "Starting miner for $USER on $CORES cores..."
 
 # Start python-skriptet
-python3 miner.py "$USER" "$KEY" "$CORES"
+python3 miner.py "$USER" "$KEY" "$CORES" "$MINER"
